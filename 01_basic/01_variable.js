a4b51f1eb05218de7ed73159c3007fff6811681b
Prince Yadav <133868722+Prince1599@users.noreply.github.com>
2025-09-21 13:02:32 +0000

    study about variable , datatype and conversionOpertions

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -1,7 +1,19 @@
 const accountId = 12545
 let accountEmail = "prince@gmail.com"
 var accountPassword = "12345"
+accountCity = "Pune"
 
-accountId = 12586
+//accountId = 12586  // not allowed
+accountEmail = "yadav@gmail.com"
+accountPassword = "2345"
+accountCity = "Gorakhpur"
+let accountState;
+
 
 console.log("accountId");
+
+/* Preder not to use var
+because of issue in black scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,70 @@
+let score = "33abc" // check null and undefined
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//Explain
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+//---------------------------------------------------------------------------------------
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//Explain
+//1 => true ; 0=> false
+//"" => false
+// "Hitesh" => true
+
+//----------------------------------------------------------------------------------------
+
+let someNumber  = 33;
+let stringSomeNumber = String(someNumber)
+// console.log(stringSomeNumber)
+// console.log(typeof stringSomeNumber)
+
+// ****************************************OPERATIONS*******************************************************************
+
+let value  = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(3+2)
+// console.log(3-2)
+// console.log(3*2)
+// console.log(3**2)
+
+let str1 = "Hello"
+let str2 = " Prince"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2); //ans 12 which is string
+// console.log("1" + "2"); //ans 12 which is string
+// console.log(1 + "2"); //ans 12 which is string
+// console.log(1 + 2 + "2"); //ans 32 which is first number and then string
+
+
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
